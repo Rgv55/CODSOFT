f149497d22f065426631f963432e7c14fc2d5385
Rgv55 <147893666+Rgv55@users.noreply.github.com>
2023-11-13 16:34:32 +0530

    Add files via upload

---
+++ Quiz/quiz_data.py
@@ -0,0 +1,25 @@
+quiz_data = [
+{
+    "question":"What is the capital of India..?",
+    "choices": ["Chennai", "Kolkata", "Delhi", "Mumbai"],
+    "answer": "Delhi"
+
+
+},
+{
+    "question": "who is the father of India..?",
+    "choices": ["Gandhi", "Nehru", "Patel", "Bose"],
+    "answer": "Gandhi"
+},
+{
+    "question": "Which is the closest planet to Earth..?",
+    "choices": ["Venus", "Mars", "Mercury", "Jupiter"],
+    "answer": "Venus"
+},
+{
+    "question": "When did India got it's Independance..?",
+    "choices": ["1945", "1946", "1947", "2003"],
+    "answer": "1947"
+}
+
+]
